d9d9bf78a348d4ba4a35c1a6ab85119a287c840d
Jon <jdgreazel@gmail.com>
2020-08-27 09:04:09 -0500

    first chunk of work

---
+++ backend/app/api/dragon.js
@@ -0,0 +1,20 @@
+const { Router } = require("express");
+const DragonTable = require("../dragon/table");
+
+const router = new Router();
+
+router.get("/new", (req, res) => {
+  const dragon = req.app.locals.engine.generation.newDragon();
+
+  DragonTable.storeDragon(dragon)
+    .then(({ dragonId }) => {
+      console.log("dragonId: ", dragonId);
+
+      dragon.dragonId = dragonId;
+
+      res.json({ dragon });
+    })
+    .catch((error) => console.error(error));
+});
+
+module.exports = router;
