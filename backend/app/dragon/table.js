a7b7062ce76d0c1f1625b7391ffefc222ecce923
Jon <jdgreazel@gmail.com>
2020-08-30 12:49:16 -0500

    finish back end and helper functions

--- backend/app/dragon/table.js
+++ backend/app/dragon/table.js
@@ -24,5 +24,21 @@ class DragonTable {
       );
     });
   }
+
+  static getDragon({dragonId}){
+    return new Promise((resolve, reject)=>{
+      pool.query(
+        'SELECT birthdate, nickname, "generationId" FROM dragon WHERE dragon.id = $1',
+        [dragonId],
+        (error, response) => {
+          if (error) return reject(error);
+          if(response.rows.length === 0) return reject(new Error('no dragon'));
+
+          resolve(response.rows[0]);
+        }
+      )
+    });
+  }
 }
+
 module.exports = DragonTable;
