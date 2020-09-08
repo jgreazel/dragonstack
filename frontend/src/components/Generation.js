f1dbbe82ea39083cfe6576a4764662a7b3b0680b
Jon <jdgreazel@gmail.com>
2020-09-07 22:28:56 -0500

    add cors, dynamic data on fe working

--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -1,16 +1,39 @@
 import React, {Component} from 'react';
 
+const DEFAULT_GENERATION = { generationId: '', expiration: ''};
+const MINIMUM_DELAY = 3000;
+
 class Generation extends Component{
 
-    state = {generation: {generationId: 69, expiration: '2022-04-01'}}; //dummy data for testing
+    state = {generation: DEFAULT_GENERATION}; 
+    timer = null;
 
     componentDidMount(){
-        this.fetchGeneration();
+        this.fetchNextGeneration();
+    }
+
+    componentWillUnmount(){
+        clearTimeout(this.timer);
     }
 
     fetchGeneration = () => {
         fetch('http://localhost:3000/generation')
-            .then(response => console.log(response));
+            .then(response => response.json())
+            .then(json => {
+                this.setState({ generation: json.generation});
+            })
+            .catch(error => console.error('error', error));
+    };
+
+    fetchNextGeneration = () =>{
+        this.fetchGeneration();
+        let delay = new Date(this.state.generation.expiration).getTime() - new Date().getTime();
+        if(delay < MINIMUM_DELAY){
+            delay = MINIMUM_DELAY;
+        }
+        this.timer = setTimeout(()=>{
+            this.fetchNextGeneration();
+        }, delay)
     }
 
     render(){
